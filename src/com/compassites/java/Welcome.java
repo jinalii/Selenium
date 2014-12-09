1cb054232a927f28b36532c9c3223f8de99a2606
Jinali <jinali.jain@compassitesinc.com>
2014-12-09 13:31:45 +0530

    My Initial Commit

---
+++ src/com/compassites/java/Welcome.java
@@ -0,0 +1,14 @@
+package com.compassites.java;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			System.out.println("Welcome");
+			
+			welcome_performance_system wps = new welcome_performance_system();
+			wps.welcomeMsg();
+	}
+
+}
---
+++ src/com/compassites/java/welcome_performance_system.java
@@ -0,0 +1,8 @@
+package com.compassites.java;
+
+public class welcome_performance_system {
+
+		public void welcomeMsg(){
+			System.out.println("Welcome Performance Testing");
+		}
+}
